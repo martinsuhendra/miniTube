84652ba802efe8bd7eb8d8edaa914c7680c1dd98
Michael <msaputra93@yahoo.com>
2019-04-11 18:56:13 +0700

    add server, create, find

--- server/app.js
+++ server/app.js
@@ -4,12 +4,15 @@ const app = express()
 const port = 3000
 const cors = require('cors')
 const mongoose = require('mongoose')
+const router = require('./routes')
 
 mongoose.connect(`${process.env.DB_URL}`, { useNewUrlParser: true })
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
+
+app.use('/', router)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
